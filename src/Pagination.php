b5e05a944680fc3c06514e130f3faae29a8b52c1
Jarzon <j@masterj.net>
2018-01-31 22:46:53 -0500

    Add Pagination class with tests

---
+++ src/Pagination.php
@@ -0,0 +1,89 @@
+<?php
+namespace Jarzon;
+
+class Pagination
+{
+    protected $currentPage = 0;
+    protected $numberOfPages = 1;
+    protected $numberOfElements = 1;
+    protected $elementsPerPages = 1;
+    protected $showPagesNumber = 1;
+    protected $backward = false;
+
+    function __construct(int $currentPage, int $numberOfElements, int $elementsPerPages, int $showPagesNumber = 3, bool $backward = false)
+    {
+        $this->numberOfElements = $numberOfElements;
+        $this->elementsPerPages = $elementsPerPages;
+        $this->showPagesNumber = $showPagesNumber;
+
+        $this->numberOfPages = ceil($numberOfElements / $elementsPerPages);
+
+        $currentPage = intval($currentPage);
+        if ($currentPage == 0) {
+            $currentPage = 1;
+        }
+
+        // Check currentPage
+        if ($currentPage < 1) {
+            $this->numberOfPages = 1;
+        }
+        if ($currentPage > $this->numberOfPages && $this->numberOfPages != 0) {
+            $currentPage = $this->numberOfPages;
+        }
+
+        $this->currentPage = $currentPage;
+        $this->backward = $backward;
+    }
+
+    function getNumberPages() : int
+    {
+        return $this->numberOfPages;
+    }
+
+    function getPage() : int
+    {
+        return $this->currentPage;
+    }
+
+    function getFirstPageElement() : int
+    {
+        $result = ($this->currentPage - 1) * $this->elementsPerPages;
+
+        if($this->backward) {
+            $result = $this->numberOfElements - ($result + $this->elementsPerPages);
+        }
+
+        return $result + 1;
+    }
+
+    function getLast() : int
+    {
+        $result = ($this->currentPage - 1) * $this->elementsPerPages;
+
+        if($this->backward) {
+            $result = $this->numberOfElements - $result;
+        } else {
+            $result += $this->elementsPerPages;
+        }
+
+        return $result;
+    }
+
+    function getElementsPerPages() : int
+    {
+        return $this->elementsPerPages;
+    }
+
+    function showPages() : string
+    {
+        $output = '';
+        for($current = ($this->currentPage - 3), $stop = ($this->currentPage + $this->showPagesNumber); $current < $stop; ++$current)
+        {
+            if($current < 1 || $current > $this->numberOfPages) continue;
+            else if($current != $this->currentPage) $output .= '<a href="'.$current.'" class="pageNumberLink">'.$current.'</a>';
+            else $output .= '<span class="pageNumber">'.$current.'</span>';
+        }
+
+        return $output;
+    }
+}
---
+++ tests/paginatorTest.php
@@ -0,0 +1,65 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+use \Jarzon\Pagination;
+
+class PaginatorTest extends TestCase
+{
+    public function testGetNumberPages()
+    {
+        $pagination = new Pagination(1, 100, 10, 3, false);
+
+        $this->assertEquals(10, $pagination->getNumberPages());
+
+        return $pagination;
+    }
+
+    /**
+     * @depends testGetNumberPages
+     */
+    public function testGetPage($pagination)
+    {
+        $this->assertEquals(1, $pagination->getPage());
+    }
+
+    /**
+     * @depends testGetNumberPages
+     */
+    public function testGetFirstPageElement($pagination)
+    {
+        $this->assertEquals(1, $pagination->getFirstPageElement());
+    }
+
+    /**
+     * @depends testGetNumberPages
+     */
+    public function testGetLast($pagination)
+    {
+        $this->assertEquals(10, $pagination->getLast());
+    }
+
+    public function testReverse()
+    {
+        $pagination = new Pagination(2, 100, 10, 3, true);
+
+        $this->assertEquals(10, $pagination->getNumberPages());
+
+        return $pagination;
+    }
+
+    /**
+     * @depends testReverse
+     */
+    public function testReverseGetFirstPageElement($pagination)
+    {
+        $this->assertEquals(81, $pagination->getFirstPageElement());
+    }
+
+    /**
+     * @depends testReverse
+     */
+    public function testReverseGetLast($pagination)
+    {
+        $this->assertEquals(90, $pagination->getLast());
+    }
+}
